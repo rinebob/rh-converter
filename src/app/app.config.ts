315c5a0fd2825c1fdca88ffee09ee92c3158a591
rinebob <rinebob111185@gmail.com>
2025-06-19 11:56:54 -0700

    Configures app.config.ts to use emulators in development mode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { ApiInterceptor } from './core/interceptors/api.interceptor';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app'; 
-import { getAuth, provideAuth } from '@angular/fire/auth'; 
-import { getFirestore, provideFirestore } from '@angular/fire/firestore'; 
+import { getAuth, provideAuth, connectAuthEmulator } from '@angular/fire/auth'; 
+import { getFirestore, provideFirestore, connectFirestoreEmulator } from '@angular/fire/firestore'; 
 import { environment } from '../environments/environment';
 import { AuthService } from './core/services/auth.service';
 
@@ -18,8 +18,20 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideFirebaseApp(() => initializeApp(environment.firebase)), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore()), 
+    provideAuth(() => {
+      const auth = getAuth();
+      if (!environment.production) {
+        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+      }
+      return auth;
+    }), 
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (!environment.production) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore;
+    }), 
     MatSnackBar,
     AuthService,
     {
